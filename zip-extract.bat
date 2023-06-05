04f6f8a912da851e1b56f0cfc9a38ab7de2889b1
mrpepper023 <yakizacana@gmail.com>
2023-06-05 17:36:16 +0900

    zip-extract.bat を更新

--- zip-extract.bat
+++ zip-extract.bat
@@ -1,5 +1,5 @@
 @powershell -NoProfile -ExecutionPolicy Unrestricted "$s=[scriptblock]::create((gc 
-¥"%"f0¥"|?{$_.readcount -gt 1})join¥"`n¥");&$s" %*&goto:eof
+¥"%"f0¥"|?{$_.readcount -gt 1})-join¥"`n¥");&$s" %*&goto:eof
 # ここからPowerShellスクリプト
 $currentTime = [System.DateTime]::Now
 
